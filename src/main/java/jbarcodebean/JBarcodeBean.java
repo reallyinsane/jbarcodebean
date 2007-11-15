f13e905e42e66c1c745b06ba55ec539ac2cfb9ba
Matthias Hanisch <hanisch.matthias@googlemail.com>
2007-11-15 19:23:10 +0000

    mhanisch ADD ============ - re-create JBarcodeBean and JBarcodeBeanBeanInfo for compatibility
    git-svn-id: https://svn.code.sf.net/p/jbarcodebean/code/branches/maven/jbarcodebean@57 cc500d36-4f3c-0410-994d-fb3de4d1fe43

---
+++ src/main/java/jbarcodebean/JBarcodeBean.java
@@ -0,0 +1,39 @@
+/**
+ *  This library is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License (LGPL) as
+ *  published by the Free Software Foundation; either version 3.0 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details. 
+ */
+
+/**
+ * Title:        JBarcodeBean
+ * Description:  Barcode JavaBeans Component
+ * Copyright:    Copyright (C) 2004
+ * Company:      Dafydd Walters
+ */
+package jbarcodebean;
+
+import net.sourceforge.jbarcodebean.model.BarcodeStrategy;
+
+/**
+ * 
+ * @author <a href="mailto:mhanisch@sourceforge.net">Matthias Hanisch</a>
+ * @deprecated As of JBarcodeBean 1.2 replaced by net.sourceforge.jbarcodebean.JBarcodeBean.
+ */
+public class JBarcodeBean extends net.sourceforge.jbarcodebean.JBarcodeBean {
+
+    public JBarcodeBean(String code, BarcodeStrategy codeType) {
+        super(code, codeType);
+        // TODO Auto-generated constructor stub
+    }
+
+    public JBarcodeBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
